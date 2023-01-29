a7f7d60fec1ff6f02985cd9770011ce555d5132f
Dmitrii Demin <mail@demin.co>
2023-01-29 15:34:47 -0600

    Updates

--- Python/tools/files/compare_folders/run.py
+++ Python/tools/files/compare_folders/run.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from argparse import ArgumentParser
-from os import listdir
-from yaml import dump
+from os import remove, path, walk, rmdir, listdir
 from hashlib import sha256
 
 
@@ -11,69 +10,81 @@ def get_digest(file_path):
         return sha256(file.read()).hexdigest()
 
 
-def check_hash(files, args):
-    print('\nChecking hash')
-
-    reply = list()
-    status = False
-
-    for file in files['left']:
-        fl = dict()
-
-        for side in files:
-            fl[side] = get_digest(f"{getattr(args, side)}/{file}")
-
-        if fl['left'] != fl['right']:
-            status = True
-            reply.append(file)
-
-    if not status:
-        print('\nHashes are equal')
-    else:
-        print('\nDifferent hashes')
-        print(dump(reply))
-
-
 def main():
     parser = ArgumentParser()
     parser.add_argument('--left', required=True, action='store')
     parser.add_argument('--right', required=True, action='store')
+    parser.add_argument('--silent', required=False, action='store_true')
 
     args = parser.parse_args()
 
-    files = dict(left=listdir(args.left),
-                 right=listdir(args.right))
+    dirs = dict(left=r'',
+                right=r'')
 
-    status = uniq_files(files)
-    if status:
-        exit(0)
+    if not dirs:
+        dirs = dict(left=args.left,
+                    right=args.right)
+    objects = dict(left=[it for sl in [[path.join(i[0], j) for j in i[2]] for i in walk(dirs['left'])] for it in sl],
+                   right=[it for sl in [[path.join(i[0], j) for j in i[2]] for i in walk(dirs['right'])] for it in sl])
 
-    check_hash(files, args)
+    for s in ['left', 'right']:
+        for f in range(len(objects[s])):
+            objects[s][f] = objects[s][f].replace(dirs[s], '')
 
+    uniq_left = list()
+    uniq_right = list()
+    shared = list()
 
-def uniq_files(files):
-    print('\nChecking uniq')
+    for item in objects['left']:
+        if item in objects['right']:
+            shared.append(item)
+        else:
+            uniq_left.append(item)
 
-    uniq = dict(left=list(),
-                right=list())
+    for item in objects['right']:
+        if item not in objects['left']:
+            uniq_right.append(item)
 
-    for k, v in files.items():
-        opposite = 'right' if k == 'left' else 'left'
-        for file in v:
-            if file not in files[opposite]:
-                uniq[k].append(file)
+    if uniq_left:
+        print(f'uniq left: {len(uniq_left)}')
 
-    status = False
+    if uniq_right:
+        print(f'uniq right: {len(uniq_right)}')
 
-    for k, v in uniq.items():
-        if v:
-            status = True
-            print(f"{k}: \n{dump(v)}")
+    if shared:
+        print(f'shared: {len(shared)}')
+        print(f'\nChecking shared')
 
-    if not status:
-        print('\nUniq are not found')
+        identical = list()
+        different = list()
 
-    return status
+        for item in shared:
+            if get_digest(f"{dirs['left']}/{item}") == get_digest(f"{dirs['right']}/{item}"):
+                identical.append(item)
+            else:
+                different.append(item)
+
+        print(f'\nidentical: {len(identical)}')
+        print(f'different: {len(different)}')
+
+        if len(identical) > 0:
+            if not args.silent:
+                if input('Delete?(y/n)') != 'y':
+                    print('\nExiting')
+                    exit()
+
+            print('\nDeleting')
+            for file in identical:
+                remove(f"{dirs['right']}{file}")
+
+            dirs_to_delete = list()
+            for pth, dirs, files in walk(dirs['right']):
+                dirs_to_delete.append(pth)
+
+            dirs_to_delete.reverse()
+            for d in dirs_to_delete:
+                if len(listdir(d)) == 0:
+                    rmdir(d)
 
 
 if __name__ == '__main__':
