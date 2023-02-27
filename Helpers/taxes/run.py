2e08923d38f20070496ff9e45b034539ff8f25fb
Dmitrii Demin <mail@demin.co>
2023-02-26 20:12:58 -0600

    Add 2023

--- Helpers/taxes/run.py
+++ Helpers/taxes/run.py
@@ -4,24 +4,44 @@ from yaml import load, CLoader
 
 
 class Taxes:
-    brackets = {2021: {10: (0, 19900),
-                       12: (19900, 81050),
-                       22: (81050, 172750),
-                       24: (172750, 329850),
-                       32: (329850, 418850),
-                       35: (418850, 628300)},
-                2022: {10: (0, 20500),
-                       12: (20550, 83550),
-                       22: (83550, 178150),
-                       24: (178150, 340100),
-                       32: (340100, 431900),
-                       35: (431900, 647850)}}
+    brackets = {
+        2021: {
+            10: (0, 19900),
+            12: (19900, 81050),
+            22: (81050, 172750),
+            24: (172750, 329850),
+            32: (329850, 418850),
+            35: (418850, 628300)
+        },
+        2022: {
+            10: (0, 20550),
+            12: (20551, 83550),
+            22: (83551, 178150),
+            24: (178151, 340100),
+            32: (340101, 431900),
+            35: (431901, 647850)
+        },
+        2023: {
+            10: (0, 22000),
+            12: (22001, 89450),
+            22: (89451, 190750),
+            24: (190751, 364200),
+            32: (364201, 462500),
+            35: (462501, 693750)
+        }
+    }
 
-    oasdi = {2021: 142800,
-             2022: 147000}
+    oasdi = {
+        2021: 142800,
+        2022: 147000,
+        2023: 160200
+    }
 
-    standard_deduction = {2021: 25100,
-                          2022: 25900}
+    standard_deduction = {
+        2021: 25100,
+        2022: 25900,
+        2023: 27700
+    }
 
     # 2021, 2022 https://www.irs.gov/newsroom/irs-announces-401k-limit-increases-to-20500
     limits_401 = {2021: 19500,
@@ -58,22 +78,71 @@ class Taxes:
                 elif profile['401k']['value'] == 'max':
                     profile['401k'] = self.limits_401[self.year]
 
-            if 'insurance' in profile:
-                tmp = 0
-                for _, v in profile['insurance']['value'].items():
-                    tmp += v
-                if profile['insurance']['pay_frequency'] == 'semi-monthly':
-                    profile['insurance'] = tmp * 24
-                elif profile['insurance']['pay_frequency'] == 'monthly':
-                    profile['insurance'] = tmp * 12
+            if 'fsa' in profile:
+                if profile['fsa']['pay_frequency'] == 'semi-monthly':
+                    profile['fsa'] = profile['fsa']['value'] * 24
+                elif profile['fsa']['pay_frequency'] == 'monthly':
+                    profile['fsa'] = profile['fsa']['value'] * 12
+                elif profile['fsa']['pay_frequency'] == 'annually':
+                    profile['fsa'] = profile['fsa']['value']
                 else:
                     exit(1)
+
+            if 'insurance' in profile:
+                tmp = 0
+                for k, v in profile['insurance']['value'].items():
+                    if k in [
+                        'dental',
+                        'medical',
+                        'vision'
+                    ]:
+                        if 'insurance_required' not in profile:
+                            profile['insurance_required'] = 0
+
+                        profile['insurance_required'] += v
+                    else:
+                        if 'insurance_user' not in profile:
+                            profile['insurance_user'] = 0
+
+                        profile['insurance_user'] += v
+
+                    tmp += v
+
+                for k in ['insurance_required', 'insurance_user']:
+                    if k in profile:
+                        if profile['insurance']['pay_frequency'] == 'semi-monthly':
+                            profile[k] *= 24
+                        elif profile['insurance']['pay_frequency'] == 'monthly':
+                            profile[k] *= 12
+                        elif profile['insurance']['pay_frequency'] == 'annually':
+                            pass
+                if profile['insurance']['pay_frequency'] == 'semi-monthly':
+                    tmp *= 24
+                elif profile['insurance']['pay_frequency'] == 'monthly':
+                    tmp *=12
+                elif profile['insurance']['pay_frequency'] == 'annually':
+                    pass
+                else:
+                    exit(1)
+
+                profile['insurance'] = tmp
 
             if 'w4_4c' in profile:
                 if profile['w4_4c']['pay_frequency'] == 'semi-monthly':
                     profile['w4_4c'] = int(profile['w4_4c']['value']) * 24
                 elif profile['w4_4c']['pay_frequency'] == 'annually':
                     profile['w4_4c'] = int(profile['w4_4c']['value'])
+                else:
+                    exit(1)
+
+            if 'income_limit' in profile:
+                average_per_period = profile['income'] / profile['income_limit']['period']
+                if profile['income_limit']['pay_frequency'] == 'semi-monthly':
+                    profile['part_income'] = average_per_period * 24
+                    profile['income_factor'] = profile['income_limit']['period'] / 24
+                elif profile['income_limit']['pay_frequency'] == 'bi-weekly':
+                    profile['part_income'] = average_per_period * 26
+                    profile['income_factor'] = profile['income_limit']['period'] / 26
                 else:
                     exit(1)
 
@@ -97,11 +166,21 @@ class Taxes:
 
         for profile in self.profiles:
             agi += profile['income']
+
             if 'self_employment' in profile and profile['self_employment'] is True:
                 social_security, medicare = self._fica(profile)
                 agi = agi - social_security - medicare
                 tax = tax + social_security + medicare
                 deduction_custom = 3165
+
+            if 'fsa' in profile:
+                agi -= profile['fsa']
+
+            if 'insurance' in profile:
+                agi -= profile['insurance']
+
+            if '401k' in profile:
+                agi -= profile['401k']
 
         tax_base = agi - deduction_base - deduction_custom
         for percent, bracket in self.brackets[self.year].items():
@@ -109,7 +188,7 @@ class Taxes:
                 step = (tax_base - bracket[0]) * percent / 100
             else:
                 step = (bracket[1] - bracket[0]) * percent / 100
-            tax = tax + step
+            tax += step
             if tax_base < bracket[1]:
                 break
 
@@ -121,26 +200,42 @@ class Taxes:
         agi = 0
         tax = 0
 
-        agi += profile['income']
+        if 'part_income' in profile:
+            agi += profile['part_income']
+        else:
+            agi += profile['income']
+
         if 'self_employment' in profile and profile['self_employment'] is True:
             social_security, medicare = self._fica(profile)
             agi = agi - social_security - medicare
             tax = tax + social_security + medicare
             deduction_custom = 3165
 
-        tax_base = agi - deduction_base - deduction_custom
+        if 'insurance_required' in profile:
+            agi -= profile['insurance_required']
+
+        if '401k' in profile:
+            agi -= profile['401k']
+
+        if agi < deduction_base:
+            tax_base = agi
+        else:
+            tax_base = agi - deduction_base - deduction_custom
 
         for percent, bracket in self.brackets[self.year].items():
             if tax_base < bracket[1]:
                 step = (tax_base - bracket[0]) * percent / 100
             else:
                 step = (bracket[1] - bracket[0]) * percent / 100
-            tax = tax + step
+            tax += step
             if tax_base < bracket[1]:
                 break
 
         if 'w4_4c' in profile:
-           tax = tax + profile['w4_4c']
+            tax += profile['w4_4c']
+
+        if 'income_limit' in profile:
+            tax *= profile['income_factor']
 
         return round(tax, 2)
 
@@ -152,21 +247,23 @@ class Taxes:
             return profile['income'] * 12.4 / 100, profile['income'] * 2.9 / 100
 
         payment = profile['income'] / 24
-        tax_base = payment if 'insurance' not in profile else payment - profile['insurance'] / 24
+        tax_base = payment if 'insurance_required' not in profile else payment - profile['insurance_required'] / 24
+        if 'fsa' in profile:
+            tax_base -= profile['fsa'] / 24
 
         ytd_payment = 0
 
         social_security = 0
         medicare = 0
         for period in range(1, 25):
-            ytd_payment = ytd_payment + payment
+            ytd_payment += payment
             if ytd_payment < self.oasdi[self.year]:
-                social_security = social_security + tax_base * 6.2 / 100
+                social_security += tax_base * 6.2 / 100
                 medicare = medicare + tax_base * 1.45 / 100
             else:
                 medicare = medicare + tax_base * 1.45 / 100
 
-        return social_security, medicare
+        return round(social_security, 2), round(medicare, 2)
 
     def _nice_print(self, output):
         for k, v in output.items():
@@ -186,6 +283,15 @@ class Taxes:
             self.nice_output_value2 = self.nice_output_value2 if self.nice_output_value2 > v2 else v2
             self.nice_output_separator = self.nice_output_key + self.nice_output_value1 + self.nice_output_value2 + 10
 
+    @staticmethod
+    def _w2_income_per_paycheck(profile):
+        result = profile['income']
+        for k in ['401k', 'insurance_required', 'fsa']:
+            if k in profile:
+                result -= profile[k]
+
+        return result
+
     def calculate(self, profiles):
         with open(profiles, mode='r') as fl:
             self.profiles = [v for _, v in load(fl.read(), Loader=CLoader)[self.year].items()]
@@ -193,6 +299,7 @@ class Taxes:
         self._adjust_profiles()
 
         output = {'Gross income': 0,
+                  'W2 income': 0,
                   'Deductions': 0,
                   '401k': 0,
                   'Insurance': 0,
@@ -211,8 +318,13 @@ class Taxes:
             income = profile['income']
             social_security, medicare = self._fica(profile)
 
+            # Gross income
             output[f'gross income {i}'] = income
             output['Gross income'] += output[f'gross income {i}']
+
+            # W2 income
+            output[f'w2 income {i}'] = self._w2_income_per_paycheck(profile)
+            output['W2 income'] += output[f'w2 income {i}']
 
             # Deductions
             output[f'deductions {i}'] = 0
@@ -319,7 +431,7 @@ class Taxes:
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--profiles', default='private.yml', type=str, action='store')
-    parser.add_argument('--year', default=2022, type=int, action='store')
+    parser.add_argument('--year', default=2023, type=int, action='store')
     args = parser.parse_args()
 
     taxes = Taxes(args.year)
